390daf0395374eaae679c40025176a06fa1afa67
adrian <nairodie@protonmail.com>
2025-04-26 14:41:07 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var a  = "Stress";
+        //Console.WriteLine(a[3]);
+
+        foreach(char item in a)
+        {
+            if (item.ToString() == "s" || item.ToString() == "S")
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
