21716cb14c64584a3f44cb4b62d652bcfc42570a
adrian <nairodie@protonmail.com>
2025-04-27 12:02:41 -0500

    The four sisters and the duckbear

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program
+﻿using System.IO.IsolatedStorage;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        Program p = new Program();
+        p.Duckbear();
+
         var a  = "Stress";
         //Console.WriteLine(a[3]);
 
@@ -11,6 +16,28 @@
             {
                 Console.WriteLine(item);
             }
+        }
+    }
+
+    public void Duckbear()
+    {
+        Console.WriteLine("How many eggs were gathered today?");
+        string? input = Console.ReadLine();
+        if (input != null && int.TryParse(input, out int totalEggs))
+        {
+              //sisters
+            int numberOfSisters = 4;
+
+            //how many eggs each sister gets and how many are leftover for the duckbear
+            int sisterEggs = totalEggs / numberOfSisters;
+            int duckbear = totalEggs % numberOfSisters;
+
+            Console.WriteLine($"Each sister gets {sisterEggs} chocolate eggs.");
+            Console.WriteLine($"The pet gets {duckbear} chocolate eggs as the remainder.");
+        }
+        else
+        {
+            Console.WriteLine($"Please enter a valid number");
         }
     }
 }
--- obj/Debug/net8.0/Practice.AssemblyInfo.cs
+++ obj/Debug/net8.0/Practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5d850479b4104748fb9d4e18889150d273cf27b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+390daf0395374eaae679c40025176a06fa1afa67")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
