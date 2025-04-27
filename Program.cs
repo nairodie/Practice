6ec9fd743669966cb6a2d9a0f229a485d5fa6c5d
adrian <nairodie@protonmail.com>
2025-04-27 14:19:50 -0500

    The Dominion of Kings

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
-﻿using System.IO.IsolatedStorage;
+﻿using System.ComponentModel;
+using System.IO.IsolatedStorage;
 using Microsoft.VisualBasic;
 
-internal class Program
+public class Program
 {
     private static void Main(string[] args)
     {
-        VariableShop();
+        //Duckbear();
+        //VariableShop();
+        Kings();
 
         var a  = "Stress";
         //Console.WriteLine(a[3]);
@@ -48,7 +51,7 @@ internal class Program
         }
     }
 
-    public void Duckbear()
+    public static void Duckbear()
     {
         Console.WriteLine("How many eggs were gathered today?");
         string? input = Console.ReadLine();
@@ -67,6 +70,78 @@ internal class Program
         else
         {
             Console.WriteLine($"Please enter a valid number");
+        }
+    }
+
+    public static void Kings()
+    {
+        string player1 = "Melik";
+        string player2 = "Casik";
+        string player3 = "Balik";
+
+        // Estate, Duchy, and Province points
+        int estatePoints = 1;
+        int duchyPoints = 3;
+        int provincePoints = 6;
+
+        // Declare variables to store holdings for each player
+        int player1Estate, player2Estate, player3Estate;
+        int player1Duchy, player2Duchy, player3Duchy;
+        int player1Province, player2Province, player3Province;
+
+        // Get the number of holdings for each player
+        Console.WriteLine($"{player1}, Enter your Estate holdings (worth {estatePoints} point each):");
+        player1Estate = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{player2}, Enter your Estate holdings (worth {estatePoints} point each):");
+        player2Estate = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{player3}, Enter your Estate holdings (worth {estatePoints} point each):");
+        player3Estate = int.Parse(Console.ReadLine());
+
+        // Get Duchy holdings
+        Console.WriteLine($"{player1}, Enter your Duchy holdings (worth {duchyPoints} points each):");
+        player1Duchy = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{player2}, Enter your Duchy holdings (worth {duchyPoints} points each):");
+        player2Duchy = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{player3}, Enter your Duchy holdings (worth {duchyPoints} points each):");
+        player3Duchy = int.Parse(Console.ReadLine());
+
+        // Get Province holdings
+        Console.WriteLine($"{player1}, Enter your Province holdings (worth {provincePoints} points each):");
+        player1Province = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{player2}, Enter your Province holdings (worth {provincePoints} points each):");
+        player2Province = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{player3}, Enter your Province holdings (worth {provincePoints} points each):");
+        player3Province = int.Parse(Console.ReadLine());
+
+        // Calculate total points for each player
+        int totalPoints1 = (player1Estate * estatePoints) + (player1Duchy * duchyPoints) + (player1Province * provincePoints);
+        int totalPoints2 = (player2Estate * estatePoints) + (player2Duchy * duchyPoints) + (player2Province * provincePoints);
+        int totalPoints3 = (player3Estate * estatePoints) + (player3Duchy * duchyPoints) + (player3Province * provincePoints);
+
+        // Output the total points for each player
+        Console.WriteLine($"{player1} has {totalPoints1} points.");
+        Console.WriteLine($"{player2} has {totalPoints2} points.");
+        Console.WriteLine($"{player3} has {totalPoints3} points.");
+
+        //Winner
+        int highestPoints = Math.Max(totalPoints1, Math.Max(totalPoints2, totalPoints3));
+        if (highestPoints == totalPoints1)
+        {
+            Console.WriteLine($"{player1} Wins!");
+        }
+        else if (highestPoints == totalPoints2)
+        {
+            Console.WriteLine($"{player2} Wins!");
+        }
+        else
+        {
+            Console.WriteLine($"{player3} wins!");
         }
     }
 }
